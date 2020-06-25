bd5753eb3cdac29242996298636aad0b31eeb33d
Benn <61118590+benedictakim@users.noreply.github.com>
2020-06-25 00:50:53 -0400

    modified:   index.html -added table modified:   script.js -added pseudocode modified:   style.css -added height to .hour

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-var currentdaytodisplay = moment().format("dddd, MMMM Do YYYY");
+var currentdaytodisplay = moment().format("dddd, MMMM Do");
 document.getElementById("currentDay").append(currentdaytodisplay);
 
+// $(document).ready(function() {
+//     var currenttime = moment().get('hour');
+//     $('.tr').each(function() {
+//         var tablehourcell = this.id;
+//         function checkingpastcurrentfuturetime () {
+//         if (tablehourcell === currenttime) {
+//             this.addclass('present');
+//         }else{
+//         if (tablehourcell < currenttime) {
+//             this.addclass('past')
+//         }else{
+//             this.addclass('future');
+//         }
+//     });
+// });
+
+
+//click into timeblock, enter text
+
+//onclick button, save to local storage text
 
