c342b3109cfbe6b992d4b99886727dede3c90f6e
Benn <61118590+benedictakim@users.noreply.github.com>
2020-07-06 19:18:42 -0400

    modified:   index.html -replace table with bootstrap row grid colum modified:   script.js -local storage, need to debug past/present/future

--- script.js
+++ script.js
@@ -1,24 +1,42 @@
-var currentdaytodisplay = moment().format("dddd, MMMM Do");
-document.getElementById("currentDay").append(currentdaytodisplay);
+$(document).ready(function(){
+    var currentdaytodisplay = moment().format("dddd, MMMM Do");
+    document.getElementById("currentDay").append(currentdaytodisplay);
 
-// $(document).ready(function() {
-//     var currenttime = moment().get('hour');
-//     $('.tr').each(function() {
-//         var tablehourcell = this.id;
-//         function checkingpastcurrentfuturetime () {
-//         if (tablehourcell === currenttime) {
-//             this.addclass('present');
-//         }else{
-//         if (tablehourcell < currenttime) {
-//             this.addclass('past')
-//         }else{
-//             this.addclass('future');
-//         }
-//     });
-// });
+    $(".saveBtn").on("click", function(){
+        var textvalue = $(this).siblings(".description").val()
+        var id = $(this).parent().attr("id")
+        window.localStorage.setItem(id, textvalue)        
+    })
+function updatehour () {
+    var currenthour = moment().hours();
+    $(".time-block").each(function(){
+        var blockhour = $(this).attr("id")
+        if (blockhour < currenthour) {
+            $(this).addClass("past")
+        }
+        else if(blockhour = currenthour) {
+            $(this).removeClass("past")
+            $(this).addClass("present")            
+        }
+        else {
+            $(this).removeClass("past")
+            $(this).removeClass("present")
+            $(this).addClass("future")
+        }
+    })
+}
+updatehour (); 
 
+var interval = setInterval(updatehour, 15000);
+$("#9am .description").val(localStorage.getItem("9am"))
+$("#10am .description").val(localStorage.getItem("10am"))
+$("#11am .description").val(localStorage.getItem("11am"))
+$("#12pm .description").val(localStorage.getItem("12pm"))
+$("#1pm .description").val(localStorage.getItem("1pm"))
+$("#2pm .description").val(localStorage.getItem("2pm"))
+$("#3pm .description").val(localStorage.getItem("3pm"))
+$("#4pm .description").val(localStorage.getItem("4pm"))
+$("#5pm .description").val(localStorage.getItem("5pm"))
 
-//click into timeblock, enter text
-
-//onclick button, save to local storage text
+})
 
